55c4b7853f36c3aee843c901b48eaa011585de9b
Juan Carlos S. <jcsepulveda.m@gmail.com>
2026-05-01 17:19:44 -0400

    chore: remove base path configuration from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.rescriptaeditorial.cl',
-  base: '/rescriptaeditorial-web',
   output: 'static',
 });
 // Trigger deploy
