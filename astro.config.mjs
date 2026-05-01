38c08d80189f3a09f58953ae58c54a22f5330a74
Juan Carlos S. <jcsepulveda.m@gmail.com>
2026-05-01 17:17:52 -0400

    chore: set base path for deployment in astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.rescriptaeditorial.cl',
+  base: '/rescriptaeditorial-web',
   output: 'static',
 });
 // Trigger deploy
